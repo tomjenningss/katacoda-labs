a003d275a0b35c64315f14d1cc25f8773a9c9cc9
Tom Jennings <thomas.jennings@ibm.com>
2019-10-08 13:42:37 +0100

    Error to run mvn liberty run server so added export commands to script

--- getting-started-guide/pre.sh
+++ getting-started-guide/pre.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/openliberty/guide-getting-started.git
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=$JAVA_HOME/jre/bin:$PATH
 cd guide-getting-started
 docker pull open-liberty:latest
