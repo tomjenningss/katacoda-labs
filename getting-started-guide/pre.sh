086a5dc4b9d6be3c2f3139ed900d3b686d20461d
Tom Jennings <thomas.jennings@ibm.com>
2019-10-08 10:10:06 +0100

    Test: See if the script to clone getting-started-guide and install maven

---
+++ getting-started-guide/pre.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/openliberty/guide-getting-started.git
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
+
+#cd guide-getting-started
+
+docker pull open-liberty:latest
