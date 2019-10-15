245dfc5db5b1c40c1c7fcb5612d76d631333a403
Tom Jennings <thomas.jennings@ibm.com>
2019-10-15 14:00:56 +0100

    altered the pre.sh file

--- getting-started-guide/pre.sh
+++ getting-started-guide/pre.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "export JAVA_HOME=/usr" >> ~/.profile;
-touch test.txt
+
 git clone https://github.com/openliberty/guide-getting-started.git
 
-docker pull open-liberty:latest
+##docker pull open-liberty:latest
 
