24a5123d5bf54f9625ee7c297c2d13acb2c92adb
Tom Jennings <thomas.jennings@ibm.com>
2019-10-15 11:09:45 +0100

    added an echo to point to profile

--- getting-started-guide/pre.sh
+++ getting-started-guide/pre.sh
@@ -5,9 +5,10 @@
 ##export PATH=$JAVA_HOME/jre/bin:$PATH
 git clone https://github.com/openliberty/guide-getting-started.git
 ##cd guide-getting-started
-##export JAVA_HOME=$(/usr/libexec/java_home)
+##export JAVA_HOME=$(/usr/lib/jvm/java-8-openjdk-amd64)
 ##export PATH=$JAVA_HOME/jre/bin:$PATH
 
 ##mvn install liberty:run-server
 
 docker pull open-liberty:latest
+
