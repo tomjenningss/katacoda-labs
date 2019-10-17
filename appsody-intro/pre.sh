12121f4d4de9128a819a705b4c9a09cd89614bdd
Tom Jennings <thomas.jennings@ibm.com>
2019-10-17 15:38:36 +0100

    added pre start up script to install the appsody data needed for lab to work - Premiliminary test

---
+++ appsody-intro/pre.sh
@@ -0,0 +1,3 @@
+mkdir appsody-install
+wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.6/appsody_0.4.6_amd64.deb"
+sudo apt install -f /root/appsody-install/appsody_0.4.6_amd64.deb
