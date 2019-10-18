2c655cb4f1d4a37059737f0053529bc55acecd5e
Tom Jennings <thomas.jennings@ibm.com>
2019-10-18 13:57:19 +0100

    added .env file -> to root dir

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -1,6 +1,8 @@
+echo "Please wait whilst the environment is set up for you"
 npm install express
 touch env.env
 echo "PORT=30000" >> ./env.env
 mkdir appsody-install
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.6/appsody_0.4.6_amd64.deb"
 sudo apt install -f /root/appsody-install/appsody_0.4.6_amd64.deb
+echo "Start the guide!"
