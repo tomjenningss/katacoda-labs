c820bac1f19143e1e5ffa434be1c3fec20df2d87
Tom Jennings <thomas.jennings@ibm.com>
2019-10-18 13:19:01 +0100

    added .env file -> to root dir

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -1,5 +1,5 @@
 touch env.env
-echo "PORT 30000" >> ./root/env
+echo "PORT 30000" >> ./root/env.env
 mkdir appsody-install
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.6/appsody_0.4.6_amd64.deb"
 sudo apt install -f /root/appsody-install/appsody_0.4.6_amd64.deb
