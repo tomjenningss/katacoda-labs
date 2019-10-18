e27af2e49f5ff28034289ed0052c5d54f809c6c2
Tom Jennings <thomas.jennings@ibm.com>
2019-10-18 14:38:23 +0100

    changed file name to .env

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -1,7 +1,6 @@
 "Please wait whilst the environment is set up for you"
 npm install express
-touch env.env
-echo "NODE_ENV=development" >> ./env.env
+touch .env
 echo "PORT=8626" >> ./env.env
 mkdir appsody-install
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.6/appsody_0.4.6_amd64.deb"
