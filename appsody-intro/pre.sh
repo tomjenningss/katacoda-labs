33f0e88e119a046cefbdc5763b679eee58d9bdb5
Tom Jennings <thomas.jennings@ibm.com>
2019-10-18 14:34:14 +0100

    added second variable to env file

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -1,7 +1,8 @@
 "Please wait whilst the environment is set up for you"
 npm install express
 touch env.env
-echo "PORT=8626 nodejs app.js" >> ./env.env
+echo "NODE_ENV=development" >> ./env.env
+echo "PORT=8626" >> ./env.env
 mkdir appsody-install
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.6/appsody_0.4.6_amd64.deb"
 apt install -f /root/appsody-install/appsody_0.4.6_amd64.deb
