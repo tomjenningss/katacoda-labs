55914bfbdbe51355115b116cdd980ba718220318
Tom Jennings <thomas.jennings@ibm.com>
2019-10-28 14:32:03 +0000

    removed comments

--- appsody-intro/pre.sh
+++ appsody-intro/pre.sh
@@ -5,5 +5,3 @@
 wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.7/appsody_0.4.7_amd64.deb"
 apt install -f /root/appsody-install/appsody_0.4.7_amd64.deb
 
-
-##wget -P /root/appsody-install/ "https://github.com/appsody/appsody/releases/download/0.4.7/appsody_0.4.7_amd64.deb"
